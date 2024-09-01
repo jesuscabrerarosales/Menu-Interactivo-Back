c883af9e1b3fb6926d027f5dbf4a80638d42797f
jesuscabrerarosales <jesuscabrerarosales47@gmail.com>
2024-09-01 00:14:06 -0500

    first commit

---
+++ src/main/java/com/menu_interactivo/Menu_Interactivo_Back/MenuInteractivoBackApplication.java
@@ -0,0 +1,13 @@
+package com.menu_interactivo.Menu_Interactivo_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuInteractivoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuInteractivoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menu_interactivo/Menu_Interactivo_Back/MenuInteractivoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menu_interactivo.Menu_Interactivo_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuInteractivoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
